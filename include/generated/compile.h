ca3492ce0c4bf2b6cfb9072af3381f682e04bd05
yank555-lu <yank555.lu@gmail.com>
2014-05-11 00:54:17 +0200

    Samsung n9005 Update 7 Source Drop with ND3 ramfs

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun May 11 00:47:08 CEST 2014"
+#define LINUX_COMPILE_BY "yank555-lu"
+#define LINUX_COMPILE_HOST "android-dev"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
