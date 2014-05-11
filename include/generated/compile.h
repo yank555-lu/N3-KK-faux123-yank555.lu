6df834faa21fa4f9d87e0cc38ba379a71590507c
yank555-lu <yank555.lu@gmail.com>
2014-05-11 14:30:49 +0200

    repo: cleanup

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sun May 11 11:43:46 CEST 2014"
-#define LINUX_COMPILE_BY "yank555-lu"
-#define LINUX_COMPILE_HOST "android-dev"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.4.0-g7b334a0-dirty"
