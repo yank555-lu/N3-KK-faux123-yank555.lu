3d9d5dc682737d4e9fb47701ad95ff648e2644e9
yank555-lu <yank555.lu@gmail.com>
2014-07-19 20:06:38 +0200

    repo: update .gitignore

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 26 */
+/* This file is auto generated, version 28 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#26 SMP PREEMPT Sat Jul 19 19:27:49 CEST 2014"
+#define UTS_VERSION "#28 SMP PREEMPT Sat Jul 19 19:47:52 CEST 2014"
 #define LINUX_COMPILE_BY "yank555-lu"
 #define LINUX_COMPILE_HOST "android-dev"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-SM-N9005-faux123-yank555.lu-NF1-kk-tw-001b5"
+#define UTS_RELEASE "3.4.0-SM-N9005-faux123-yank555.lu-NF1-kk-tw-001b6"
