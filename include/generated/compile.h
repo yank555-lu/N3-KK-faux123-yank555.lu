1da3ecb63667cfa3e1f7a6ce177dba886b1b4546
yank555-lu <yank555.lu@gmail.com>
2014-07-27 15:30:06 +0200

    repo: update .gitignore

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 28 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#28 SMP PREEMPT Sat Jul 19 19:47:52 CEST 2014"
-#define LINUX_COMPILE_BY "yank555-lu"
-#define LINUX_COMPILE_HOST "android-dev"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
